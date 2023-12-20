54af7b40e12782923f5a68a1fba2cfaaac63157e
Shu Liu <liushu_public@yeah.net>
2023-12-20 10:54:06 +0800

    refactor util-text

--- Final/util/text.py
+++ Final/util/text.py
@@ -1,25 +1,25 @@
 import re
-import string
 from nltk.corpus import stopwords
+
 
 STOP_WORDS = stopwords.words()
 
-
-def remove_at(text: str):
-    return re.sub(r'@\w+', "", text)
-
-
-def remove_url(text: str):
-    return re.sub(
-        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
-        "",
-        text
-    )
+PATTERN = {
+    'at': r'@\w+',
+    'url': r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+    'hashtag': r'#\w+',
+    'uppercase': r'\b[A-Z]+\b',
+    'lowercase': r'\b[a-z]+\b',
+    'punc': r'[^\w\s]',
+    'emoji': r'[\U0001F300-\U0001F64F\U0001F680-\U0001F6FF\u2600-\u26FF\u2700-\u27BF]'
+}
 
 
-def remove_stopwords(tokens: list, stop_words = STOP_WORDS):
+def remove_pattern(text: str, pattern):
+    return re.sub(pattern, "", text)
+
+
+def remove_stopwords(tokens: list, stop_words: list = None):
+    stop_words = STOP_WORDS if stop_words is None else stop_words
     return [token for token in tokens if token.lower() not in stop_words + ["", " "]]
 
-
-def remove_punctuation(tokens: list, punctuation: str = string.punctuation):
-    return [token for token in tokens if token not in punctuation]
