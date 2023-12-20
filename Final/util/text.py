658a27e2fb22ed16cb5614c86759baee088327a2
Shu Liu <liushu_public@yeah.net>
2023-12-20 09:06:10 +0800

    clean util-text

--- Final/util/text.py
+++ Final/util/text.py
@@ -1,24 +1,8 @@
 import re
-import nltk
 import string
-import emoji
 from nltk.corpus import stopwords
 
 STOP_WORDS = stopwords.words()
-
-
-def formalize(text: str, stop_words=STOP_WORDS):
-    # replace emoji
-    text = emoji.demojize(text)
-    # to lowercase
-    text = text.lower()
-    # tokenize
-    tokens = nltk.tokenize.word_tokenize(text)
-    # remove stopwords
-    tokens = [token for token in tokens if token.lower() not in stop_words + ["", " "]]
-    # remove punctuation
-    tokens = [token for token in tokens if token not in string.punctuation]
-    return tokens
 
 
 def remove_at(text: str):
@@ -31,3 +15,11 @@ def remove_url(text: str):
         "",
         text
     )
+
+
+def remove_stopwords(tokens: list, stop_words = STOP_WORDS):
+    return [token for token in tokens if token.lower() not in stop_words + ["", " "]]
+
+
+def remove_punctuation(tokens: list, punctuation: str = string.punctuation):
+    return [token for token in tokens if token not in punctuation]
